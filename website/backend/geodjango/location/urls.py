a6e303a8e313f41b85fce2ef361b5a9cf9aef0bf
sara-h-chen <deutscher71112@gmail.com>
2016-10-30 23:06:17 +0000

    Create browsable API for backend

--- website/backend/geodjango/location/urls.py
+++ website/backend/geodjango/location/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from location import views
 
 urlpatterns = [
     url(r'^location/$', views.location_list),
     url(r'^location/(?P<pk>[0-9]+)/$', views.location_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
