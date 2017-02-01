620e366f87a074daf908dc6f3ad1cffcb8222b6d
sara-h-chen <deutscher71112@gmail.com>
2017-02-01 17:56:49 +0000

    Remove geodjango and Webpack; restart with plain Django REST

--- courgette/courgette/urls.py
+++ courgette/courgette/urls.py
@@ -1,4 +1,4 @@
-"""geodjango URL Configuration
+"""courgette URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^backend/', include('backend.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('location.urls')),
-]
-
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- website/backend/geodjango/location/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from location.models import UserDB
-# Register your models here.
-admin.site.register(UserDB)
