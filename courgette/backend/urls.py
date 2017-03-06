2d33bc4cb14a378218d5a1ef789b9a8b7f4fa407
tomwhittaker <thomas.j.whittaker@durham.ac.uk>
2017-03-06 12:31:28 +0000

    server would not run have now fixed error but think froegin key connection may be wrong way round

--- courgette/backend/urls.py
+++ courgette/backend/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/(?P<type>[0-4]{1})/$', views.search, name='search')
+    url(r'^courgette/(?P<userID>[0-4]{5})/$', views.notifcation, name='notifcations')
     # url(r'^(?P<username>[[a-zA-Z0-9]+)/$', views.user_page, name='user_page'),
 ]
