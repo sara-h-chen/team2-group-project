c6cbc890ce40f162dba0f2b2513eefe5750a36ed
sara-h-chen <deutscher71112@gmail.com>
2017-03-25 20:17:51 +0000

    Fix regex for location search and messages URL

--- courgette/backend/urls.py
+++ courgette/backend/urls.py
@@ -5,10 +5,10 @@ import views
 
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    url(r'^food/(?P<latitude>[^[0-9]+(\.[0-9]{1,2})?]+)/(?P<longitude>[^[0-9]+(\.[0-9]{1,2})?]+)/$', views.foodList),
+    url(r'^food/(?P<latitude>\d+\.\d{2})/(?P<longitude>\d+\.\d{2})/$', views.foodList),
     # url(r'^search/(?P<type>[0-4]{1})/$', views.search, name='search'),
     # url(r'^courgette/(?P<userID>[0-4]{5})/$', views.notifcation, name='notifcations')
-    url(r'^user/(?P<username>[a-zA-Z0-9]+)/messages$', views.getMessages),
+    url(r'^user/(?P<username>[a-zA-Z0-9]+)/messages/$', views.getMessages),
     url(r'^user/(?P<username>[a-zA-Z0-9]+)/$', views.findUser, name='findUser'),
     url(r'^accounts/login/(?P<username>[a-zA-Z0-9]+)/(?P<password>[a-zA-Z0-9]+)/$', views.authenticate),
 ]
